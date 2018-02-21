05cde378c61f6a72b7fce212586e23bdbf56a40f
root <root@ip-172-31-14-245.us-east-2.compute.internal>
2018-02-21 05:18:05 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,15 @@ class MoviesController < ApplicationController
   def index
     #@movies = Movie.all
     @movies = Movie.order(params[:sort_by])
-    #@movies = Movie.find(:all, :order => (params[:sort_by]))
-    #@movies = Movie.order(:sort_by[params])
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    end
     @sort_column = params[:sort_by]
+    @all_ratings = Movie.all_ratings
+    @set_ratings = params[:ratings]
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    
+    def self.all_ratings
+        arr = Array.new
+        self.select("rating").uniq.each {|x| arr.push(x.rating)}
+        arr.sort.uniq
+    end
 end
